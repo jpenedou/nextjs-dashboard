c6102dce9c772d5a853900cb566e128273354dab
Jose Antonio <japenedo@gmail.com>
2025-04-26 18:11:19 +0200

    chapter 16, added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | GlobalCompany Dashboard',
+    default: 'GlobalCompany Dashboard'
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-seven-omega-40.vercel.app')
+}
 
 export default function RootLayout({
   children,
