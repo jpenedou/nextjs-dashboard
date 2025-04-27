50278d0a096db94fbde14ac11f619778ab4c0673
Jose Antonio <japenedo@gmail.com>
2025-04-28 00:26:09 +0200

    titles fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 export const metadata: Metadata = {
-  title: 'Invoices',
+  title: 'Login',
 };
 
 export default function LoginPage() {
